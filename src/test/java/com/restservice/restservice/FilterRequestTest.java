3d3e9c24aff556433d4d59d992ddffd7c425c02e
Vicky Valverde <vickyvalverdeg@gmail.com>
2020-03-21 19:04:21 -0500

    Removes unused packages import

--- src/test/java/com/restservice/restservice/FilterRequestTest.java
+++ src/test/java/com/restservice/restservice/FilterRequestTest.java
@@ -1,8 +1,5 @@
 package com.restservice.restservice;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.restservice.restservice.filter.FilterRequest;
 import org.junit.Before;
@@ -11,29 +8,20 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
-import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
-import static org.mockito.internal.util.MockUtil.createMock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FilterRequestTest {
